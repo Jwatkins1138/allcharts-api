1938521d8556b53f5b3a7352728ca7a533cf7581
Jason Watkins <goushoryuken@gmail.com>
2022-08-29 10:41:35 -0700

    add key to members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -14,7 +14,8 @@ class MembersController < ApplicationController
 
   def get_user_from_token
     jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
-                  Rails.application.credentials.devise[:jwt_secret_key]).first
+                  # Rails.application.credentials.devise[:jwt_secret_key]).first
+                  ENV["SECRET_KEY_BASE"]).first
     user_id = jwt_payload['sub']
     user = User.find(user_id.to_s)
   end
