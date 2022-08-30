01a7e4001be5603900681967cb5eaad7576fa589
Jason Watkins <goushoryuken@gmail.com>
2022-08-30 13:15:47 -0700

    wrong cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -25,7 +25,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "http://allcharts.site"
+    origins "http://www.allcharts.site"
 
     resource "*",
       headers: :any,
