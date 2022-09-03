56ab90b54621eb9cb58a2d6326374ca10399485e
Jason Watkins <goushoryuken@gmail.com>
2022-09-03 13:18:41 -0700

    list functions

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,40 @@
+class ListsController < ApplicationController
+  def index
+    user = get_user_from_token
+    if user
+      lists = List.where("user_id = ?", user.id)
+      render json: {
+        lists: lists
+      }
+    end
+  end
+
+  def new
+    list = List.new
+  end
+
+  def create
+    user = get_user_from_token
+    list = user.lists.build(list_params)
+
+    if list.save
+      render json: list
+    else
+      render json: list.errors
+    end
+  end
+
+  private
+
+  def get_user_from_token
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+                  # Rails.application.credentials.devise[:jwt_secret_key]).first
+                  ENV["SECRET_KEY_BASE"]).first
+    user_id = jwt_payload['sub']
+    user = User.find(user_id.to_s)
+  end
+
+  def post_params
+    params.permit(:name, :symbols)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'member/data', to: 'members#show'
   get 'posts/index/:symbol', to: 'posts#index'
   post 'posts', to: 'posts#create'
+  get 'lists/index', to: 'lists#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
