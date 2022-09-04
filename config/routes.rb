2773813720cb22fe4029c828daf52888ae5693bd
Jason Watkins <goushoryuken@gmail.com>
2022-09-04 13:31:13 -0700

    lists route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'posts/index/:symbol', to: 'posts#index'
   post 'posts', to: 'posts#create'
   get 'lists/index', to: 'lists#index'
+  post 'lists', to: 'lists#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
