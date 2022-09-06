02d53bbb855dd3b0838741ae7985484d0f012c63
Jason Watkins <goushoryuken@gmail.com>
2022-09-06 13:39:32 -0700

    search fix

--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -1,7 +1,6 @@
 class TickersController < ApplicationController
   def index
-    tickers = Ticker.where("name = ?", params[:search]).limit(5)
-    # tickers = Ticker.limit(5)
+    tickers = Ticker.where("name = ?", "%#{params[:search]}%").limit(5)
     render json: tickers
   end
 end
