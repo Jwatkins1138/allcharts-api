c42f2b34759e63b71074d282c8d855d2faadf353
Jason Watkins <goushoryuken@gmail.com>
2022-09-06 19:15:43 -0700

    search only starts with

--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -1,6 +1,6 @@
 class TickersController < ApplicationController
   def index
-    tickers = Ticker.where("name LIKE ?", "%#{params[:search]}%").limit(5)
+    tickers = Ticker.where("name LIKE ?", "#{params[:search]}%").limit(5)
     puts params[:search]
     render json: tickers
   end
